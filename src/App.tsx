e39025d6213dfc9efbdbb8d1a4fee87a8c6146a7
Yuto Ono <stellarwindmaster@gmail.com>
2020-07-28 22:00:35 +0900

    ルーティング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import CourseList from './pages/CourseList';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <div className="navbar">
+            <Link to="/">Home</Link>
+            <Link to="/course-list">Course List</Link>
+          </div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/course-list" component={CourseList} />
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/CourseList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class CourseList extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>CourseList</h1>
+        <h2>I am Yuto. I am Perfect Human.</h2>
+      </div>
+    )
+  }
+}
+
+
+export default CourseList;
