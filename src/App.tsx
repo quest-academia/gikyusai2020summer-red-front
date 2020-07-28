af3884f15a71a529922c387caafa470995e93b87
Takashi-Funahashi <vice.job0709@gmail.com>
2020-07-28 22:49:05 +0900

    mypage inport

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './Home';
+import MyPage from './pages/MyPage';
 import CourseList from './pages/CourseList';
 
 
@@ -12,9 +13,11 @@ function App() {
         <div>
           <div className="navbar">
             <Link to="/">Home</Link>
+            <Link to="mypage">マイページ</Link>
             <Link to="/course-list">Course List</Link>
           </div>
           <Route exact path="/" component={Home} />
+          <Route exacr path="/mypage" component={MyPage} />
           <Route exact path="/course-list" component={CourseList} />
         </div>
       </Router>
---
+++ src/pages/MyPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class MyPage extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>MyPage</h1>
+        <h2>工事中</h2>
+      </div>
+    )
+  }
+}
+
+
+export default MyPage;
