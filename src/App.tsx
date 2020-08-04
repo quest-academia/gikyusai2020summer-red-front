49129c56477141048a169d4b508be8d77631cdfa
Yuto Ono <stellarwindmaster@gmail.com>
2020-08-04 21:37:08 +0900

    テストページ作ったよ！

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import MyPage from './pages/MyPage';
 import CourseList from './pages/CourseList';
+import ApiTest from './pages/ApiTest';
 
 function App() {
   return (
@@ -14,10 +15,12 @@ function App() {
             <Link to="/">Home</Link>
             <Link to="mypage">マイページ</Link>
             <Link to="/course-list">Course List</Link>
+            <Link to="/api-test">API Test</Link>
           </div>
           <Route exact path="/" component={Home} />
           <Route exacr path="/mypage" component={MyPage} />
           <Route exact path="/course-list" component={CourseList} />
+          <Route exact path="/api-test" component={ApiTest} />
         </div>
       </Router>
     </div>
---
+++ src/pages/ApiTest.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ApiTest extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>API TEST</h1>
+        <h2>I am Yuto. I am Perfect Human.</h2>
+      </div>
+    );
+  }
+}
+
+export default ApiTest;
