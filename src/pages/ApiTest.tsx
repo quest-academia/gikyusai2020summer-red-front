ffd3c43d4cd290035f6edf6f25d4989965346c5a
Yuto Ono <stellarwindmaster@gmail.com>
2020-08-04 22:46:03 +0900

    APIテストページ作成

--- src/pages/ApiTest.tsx
+++ src/pages/ApiTest.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import axious from "axios";
 
-class ApiTest extends React.Component {
+interface Props {}
+interface State {
+  json: string;
+  title: string;
+}
+
+class ApiTest extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      json: "",
+      title: "",
+    };
+    axious
+      .get("https://gikyusai-red.herokuapp.com/api/v1/quests/1")
+      .then((response: any) => {
+        console.log(response.data.data.title);
+        this.setState({
+          json: JSON.stringify(response.data, null, "    "),
+          title: response.data.data.title,
+        });
+      });
+  }
   render() {
     return (
       <div>
-        <h1>API TEST</h1>
-        <h2>I am Yuto. I am Perfect Human.</h2>
+        <h1>{this.state.title}</h1>
+        <pre
+          style={{
+            textAlign: "left",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          {this.state.json}
+        </pre>
       </div>
     );
   }
