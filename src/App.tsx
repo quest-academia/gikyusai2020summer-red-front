8eed0f0dc13c284126d8af564199e88a5ae1b303
KyoheiYamamoto <arienkyohei@gmail.com>
2020-08-18 22:07:46 +0900

    Detailページ作成

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./Home";
 import MyPage from "./pages/MyPage";
 import CourseList from "./pages/CourseList";
 import ApiTest from "./pages/ApiTest";
+import CourseDetail from "./pages/CourseDetail";
 
 function App() {
   return (
@@ -16,11 +17,13 @@ function App() {
             <Link to="mypage">マイページ</Link>
             <Link to="/course-list">Course List</Link>
             <Link to="/api-test">API Test</Link>
+            <Link to="/course-detail">詳細</Link>
           </div>
           <Route exact path="/" component={Home} />
           <Route exacr path="/mypage" component={MyPage} />
           <Route exact path="/course-list" component={CourseList} />
           <Route exact path="/api-test" component={ApiTest} />
+          <Route exact path="/course-detail" component={CourseDetail} />
         </div>
       </Router>
     </div>
--- src/pages/CourseList.tsx
+++ src/pages/CourseList.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React from 'react';
 
+import CourseDetail from './CourseDetail';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 class CourseList extends React.Component {
   render() {
     return (
       <div>
         <h1>CourseList</h1>
         <h2>I am Yuto. I am Perfect Human.</h2>
+         <Router>
+          <div className="App">
+            <div>
+              <Link to="/course-detail">詳細</Link>
+            </div>
+          </div>
+        </Router>
       </div>
     );
   }
