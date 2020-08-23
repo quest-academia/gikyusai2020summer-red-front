649d022832a351745860824bd0523551593ac274
sedoriProg <62420863+sedoriProg@users.noreply.github.com>
2020-08-23 16:06:34 +0900

    コース（クエスト？）リストのロジック部分作成

--- src/pages/CourseList.tsx
+++ src/pages/CourseList.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { RouteComponentProps } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import { RouteComponentProps } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import axious from "axios";
 
-interface Props extends RouteComponentProps<{id: string}> {}
+type Props = RouteComponentProps<{ id: string }>;
+interface ResponseData {
+  id: number;
+  title: string;
+  award: string;
+  created_at: Date;
+  updated_at: Date;
+  user_id: number;
+}
 interface State {
-  quest_id: number;
+  data: ResponseData[];
 }
 
 class CourseList extends React.Component<Props, State> {
@@ -13,12 +22,21 @@ class CourseList extends React.Component<Props, State> {
     super(props);
     let quest_id = Number(props.match.params.id);
     if (isNaN(quest_id)) {
-      quest_id = 1
+      quest_id = 1;
     }
     this.state = {
-      quest_id
+      data: [],
     };
+    (async () => {
+      const res = await axious.get(
+        "https://gikyusai-red.herokuapp.com/api/v1/quests"
+      );
+      this.setState({
+        data: res.data.data,
+      });
+    })();
   }
+
   render() {
     return (
       <div>
@@ -27,10 +45,18 @@ class CourseList extends React.Component<Props, State> {
             <Grid container alignItems="center" justify="center">
               <h1>現在進行中のクエスト</h1>
               {/* 現在進行中クエスト表示 */}
-              <a className="link-to-subquestpage" href="/1">サブクエスト1</a>
-              <a className="link-to-subquestpage" href="/2">サブクエスト2</a>
-              <a className="link-to-subquestpage" href="/3">サブクエスト3</a>
-              <a className="link-to-subquestpage" href="/4">サブクエスト4</a>
+              <a className="link-to-subquestpage" href="/1">
+                サブクエスト1
+              </a>
+              <a className="link-to-subquestpage" href="/2">
+                サブクエスト2
+              </a>
+              <a className="link-to-subquestpage" href="/3">
+                サブクエスト3
+              </a>
+              <a className="link-to-subquestpage" href="/4">
+                サブクエスト4
+              </a>
             </Grid>
           </Paper>
         </div>
@@ -38,10 +64,17 @@ class CourseList extends React.Component<Props, State> {
         <div className="QuestList">
           <h1>クエスト一覧</h1>
           {/* 選べるクエストを表示 */}
-          <a className="link-to-courcedetail" href="/course-detail/">クエスト{this.state.quest_id}</a>
-          <a className="link-to-courcedetail" href="/course-detail/">クエスト{this.state.quest_id}</a>
-          <a className="link-to-courcedetail" href="/course-detail/">クエスト{this.state.quest_id}</a>
-          <a className="link-to-courcedetail" href="/course-detail/">クエスト{this.state.quest_id}</a>
+          {this.state.data.map((d) => {
+            const url = "/course-detail/" + d.id;
+            return (
+              <p>
+                <a className="link-to-courcedetail" href={url}>
+                  {d.title}
+                  {d.id}
+                </a>
+              </p>
+            );
+          })}
         </div>
       </div>
     );
