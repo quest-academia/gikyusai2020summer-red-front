c57c6679447e07dcac1d5dadae2218cb33f7367c
KyoheiYamamoto <arienkyohei@gmail.com>
2020-08-25 22:47:58 +0900

    総合演習の欄のインデント揃えました

--- src/pages/CourseDetail.tsx
+++ src/pages/CourseDetail.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
 interface Props extends RouteComponentProps<{ id: string }> { }
-interface State
-{
+interface State {
   quest_id: number;
 }
 
 class CourseDetail extends React.Component<Props, State> {
-  constructor(props: Props)
-  {
+  constructor(props: Props) {
     super(props);
     let quest_id = Number(props.match.params.id);
     if (isNaN(quest_id)) {
-      quest_id = 1
+      quest_id = 1;
     }
     this.state = {
       quest_id
@@ -31,21 +29,26 @@ class CourseDetail extends React.Component<Props, State> {
             </div>
             <div>
               <div>
-                <ul>
-                  <a data-chapter-order="1" href="#1">1:1面とその側面１段目まで揃えよう！
-                  </a>
+                <ul className="practice">
+                 <p className="practice2">
+                    <a href="#1">1:1面とその側面1段目を揃えよう！</a>
+                  </p>
                 </ul>
-                <ul>
-                  <a data-chapter-order="2" href="#2">2:上面を揃えよう！
-                  </a>
+                <ul className="practice">
+                  <p className="practice2">
+                    <a href="#2">2:上面を揃えよう！</a>
+                  </p>
                 </ul>
-                <ul>
-                  <a data-chapter-order="3" href="#3">3:上面4隅の位置を揃えよう！
-                  </a>
+
+                <ul className="practice">
+                  <p className="practice2">
+                    <a href="#3">3:上面4隅の位置を揃えよう！</a>
+                  </p>
                 </ul>
-                <ul>
-                  <a data-chapter-order="4" href="#4">4:6面完成してみよう
-                  </a>
+                <ul className="practice">
+                  <p className="practice2">
+                    <a href="#4">4:6面完成してみよう</a>
+                  </p>
                 </ul>
               </div>
             </div>
