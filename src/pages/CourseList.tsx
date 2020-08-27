42b71d9b40ee2b294adf0fb8c308465b01359840
Takashi-Funahashi <vice.job0709@gmail.com>
2020-08-27 19:07:05 +0900

    サブクエスト非表示

--- src/pages/CourseList.tsx
+++ src/pages/CourseList.tsx
@@ -40,11 +40,13 @@ class CourseList extends React.Component<Props, State> {
   render() {
     return (
       <div>
+        {/* commentout-start @サブクエストを非表示
         <div className="ChallengingQuest">
           <Paper>
             <Grid container alignItems="center" justify="center">
               <h1>現在進行中のクエスト</h1>
-              {/* 現在進行中クエスト表示 */}
+              {// 現在進行中クエスト表示
+              }
               <a className="link-to-subquestpage" href="/1">
                 サブクエスト1
               </a>
@@ -60,21 +62,35 @@ class CourseList extends React.Component<Props, State> {
             </Grid>
           </Paper>
         </div>
-
+        commentout-end @サブクエストを非表示 */}
+        
         <div className="QuestList">
           <h1>クエスト一覧</h1>
           {/* 選べるクエストを表示 */}
-          {this.state.data.map((d) => {
-            const url = "/course-detail/" + d.id;
-            return (
-              <p>
-                <a className="link-to-courcedetail" href={url}>
-                  {d.title}
-                  {d.id}
-                </a>
-              </p>
-            );
-          })}
+          <Grid 
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: '50vh' }}
+          >
+            
+            {this.state.data.map((d) => {
+              const url = "/course-detail/" + d.id;
+              return (
+                <Paper variant="outlined" elevation={5}>
+                <p>
+                  <a className="link-to-courcedetail" href={url}>
+                    {d.title}
+                    {d.id}
+                  </a>
+                </p>
+                </Paper>
+              );
+            })}
+            
+          </Grid>
         </div>
       </div>
     );
